eb6be1f41c1d4aa6dbbce8f5af1799a5291080b2
Adam Creed <47678215+aicreed@users.noreply.github.com>
2019-10-04 22:10:48 +0100

    Added XML Helpers

---
+++ OCRProgrammingProject/XMLHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace OCRProgrammingProject
+{
+    //This file is used to create and read XML files within the app
+    public class XMLHelper
+    {
+        //This method generates the Users for the app
+        public static void GenerateAuthorisedUsers()
+        {
+            var _userList = new List<User>(); // Creates a new list for users.
+            _userList.Add(new User { Username = "adam", Password = "test" }); //Adds a new user to the list.
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<User>)); //Creates a new instance of the XML serializer with the type of List<User>
+            using (TextWriter writer = new StreamWriter(@"UserList.xml"))
+            {
+                serializer.Serialize(writer, _userList);
+            }
+        }
+    }
+}
