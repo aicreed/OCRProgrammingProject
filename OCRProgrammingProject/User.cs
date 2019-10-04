4928b1dbc848267991a59beb0de377fc0b9344f8
Adam Creed <47678215+aicreed@users.noreply.github.com>
2019-10-04 21:58:06 +0100

    Initial Commit

---
+++ OCRProgrammingProject/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRProgrammingProject
+{
+    //User Class
+    [Serializable] //This attribute allows the class to be serialized as an XML file.
+    public class User
+    {
+        //User attributes
+        public string Username { get; set; }
+        public string Password { get; set; }
+       
+    }
+}
