2e9acbdee8b6d6e101f0ccf3f7401c9ec1cee022
Adam Creed <47678215+aicreed@users.noreply.github.com>
2020-01-30 20:34:08 +0000

    Update User.cs

--- ProgrammingProject.Model/User.cs
+++ ProgrammingProject.Model/User.cs
@@ -13,5 +13,6 @@ namespace ProgrammingProject.Model
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int Score { get; set; }
     }
 }
