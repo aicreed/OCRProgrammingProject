cb4c16cbbfab3b8d211a1b9dc695e414bab7c117
Adam Creed <47678215+aicreed@users.noreply.github.com>
2020-01-31 09:30:06 +0000

    App restructure

--- ProgrammingProject/AuthenticationService.cs
+++ ProgrammingProject/AuthenticationService.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ProgrammingProject.Model;
 
-namespace ProgrammingProject.Model
+namespace ProgrammingProject
 {
     public class AuthenticationService
     {
