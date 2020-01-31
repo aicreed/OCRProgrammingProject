494e68f2dbc05d5ca044e50f17fbd60cda69e538
Adam Creed <47678215+aicreed@users.noreply.github.com>
2020-01-31 09:26:35 +0000

    Update SongGuess.cs

--- ProgrammingProject.Model/SongGuess.cs
+++ ProgrammingProject.Model/SongGuess.cs
@@ -9,47 +9,58 @@ namespace ProgrammingProject.Model
 {
     public class SongGuess
     {
+        public static int _guesses { get; set; }
         public static void Start()
         {
-            Console.WriteLine("");
-            var context = new AppDBContext();
-            var currentScore = AuthenticationService.GetScore(AuthenticationService.CurrentUser);
-
-
-            //Get Random Song
-            var song = context.Songs
-                .OrderBy(r => Guid.NewGuid()).Take(5)
-                .FirstOrDefault();
-
-
-            Console.WriteLine("Artist: " + song.Artist);
-            Console.WriteLine("Title: " + song.Name.ToCharArray()[0]);
-            Console.WriteLine("What is the song's name?");
-            Console.WriteLine("");
-            Console.Write("Guess: ");
-            var userAnswer = Console.ReadLine();
-            if (userAnswer.ToLower() == song.Name.ToLower())
+            if(_guesses != 2)
             {
-                Console.WriteLine("Correct! Press ENTER or RETURN to try another. ");
-                AuthenticationService.AddScore(AuthenticationService.CurrentUser, currentScore);
-                var enter = Console.ReadLine();
-                if (enter == "")
-                {
-                    Console.Clear();
-                    Start();
-                }
+                Console.WriteLine("");
+                var context = new AppDBContext();
+                var currentScore = AuthenticationService.GetScore(AuthenticationService.CurrentUser);
 
+
+                //Get Random Song
+                var song = context.Songs
+                    .OrderBy(r => Guid.NewGuid()).Take(5)
+                    .FirstOrDefault();
+
+
+                Console.WriteLine("Artist: " + song.Artist);
+                Console.WriteLine("Title: " + song.Name.ToCharArray()[0]);
+                Console.WriteLine("What is the song's name?");
+                Console.WriteLine("");
+                Console.Write("Guess: ");
+                var userAnswer = Console.ReadLine();
+                if (userAnswer.ToLower() == song.Name.ToLower())
+                {
+                    Console.WriteLine("Correct! Press ENTER or RETURN to try another. ");
+                    AuthenticationService.AddScore(AuthenticationService.CurrentUser, currentScore);
+                    var enter = Console.ReadLine();
+                    if (enter == "")
+                    {
+                        Console.Clear();
+                        Start();
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect. Press ENTER or RETURN to try again. ");
+                    _guesses++;
+                    var enter = Console.ReadLine();
+                    if (enter == "")
+                    {
+                        Console.Clear();
+                        Start();
+                    }
+                }
             }
             else
             {
-                Console.WriteLine("Incorrect. Press ENTER or RETURN to try again. ");
-                var enter = Console.ReadLine();
-                if (enter == "")
-                {
-                    Console.Clear();
-                    Start();
-                }
+                Console.WriteLine("Game over! Maximum amount of guesses reached. ");
+                Console.Read();
             }
+           
         }
     }
 }
