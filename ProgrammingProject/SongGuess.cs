c040f6a0f9d3dcd5b2940df60e4dc807a08e2998
Adam Creed <47678215+aicreed@users.noreply.github.com>
2020-02-01 21:10:25 +0000

    Update SongGuess.cs

--- ProgrammingProject/SongGuess.cs
+++ ProgrammingProject/SongGuess.cs
@@ -15,23 +15,25 @@ namespace ProgrammingProject
             if (_guesses != 2)
             {
                 Console.WriteLine("");
+                //Creates a new instance of the Db context and then gets the current score of the current user.
                 var context = new AppDBContext();
                 var currentScore = AuthenticationService.GetScore(AuthenticationService.CurrentUser);
 
 
-                //Get Random Song
+                //Get random song from database.
                 var song = context.Songs
                     .OrderBy(r => Guid.NewGuid()).Take(5)
                     .FirstOrDefault();
 
-
+                //Writes the artist to screen.
                 Console.WriteLine("Artist: " + song.Artist);
+                //Converts the title of the song to a char array and displays the first char.
                 Console.WriteLine("Title: " + song.Name.ToCharArray()[0]);
                 Console.WriteLine("What is the song's name?");
                 Console.WriteLine("");
                 Console.Write("Guess: ");
                 var userAnswer = Console.ReadLine();
-                if (userAnswer.ToLower() == song.Name.ToLower())
+                if (userAnswer.ToLower() == song.Name.ToLower())//Converting both to lower case - means user can input in any case and will not effect answer.
                 {
                     Console.WriteLine("Correct! Press ENTER or RETURN to try another. ");
                     AuthenticationService.AddScore(AuthenticationService.CurrentUser, currentScore);
